433f231544a3ccdead232ff04b58997a924fbc87
MwaEphraim <mwaephraim@gmail.com>
2026-09-24 19:25:52 +0200

    Initial commit

---
+++ src/main/java/com/example/hellofx/HelloJavaFX.java
@@ -0,0 +1,40 @@
+package com.example.hellofx;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class HelloJavaFX extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label message = new Label("Welcome, Muleya Mulambo!");
+        Button startButton = new Button("Start");
+        Button resetButton = new Button("Reset");
+
+        startButton.setOnAction(event -> {
+            message.setText("Great! You clicked the button.");
+        });
+
+        resetButton.setOnAction(event -> {
+            message.setText("Welcome, Muleya Mulambo!");
+                });
+
+
+        VBox layout = new VBox(20);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(message, startButton, resetButton);
+
+        Scene scene = new Scene(layout, 500, 300);
+        stage.setTitle("My First JavaFX Application - 202513222");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+    launch(args);
+    }
+}
